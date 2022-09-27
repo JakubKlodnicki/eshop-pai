0dc7eaa60bf6c228eb217f8346ff89ec18823bf2
Jakub Kłodnicki <kuba.biuro721@gmail.com>
2022-09-27 23:38:15 +0200

    nodobra

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Products from './components/products/Products';
 import Cart from './components/cart/Cart';
 import companyLogo from './logo.png';
 import cartLogo from './cart.jpg';
+import correctpromocode from './components/cart/Cart';
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -20,8 +21,26 @@ const App = () => {
     setCart([...cart, item]);
   };
 
+
+
   useEffect(() => {
     const dataPromise = fetch('products.json');
+    const categoryselect = document.getElementById('category');
+    const promocode = document.getElementById("order-select");
+    // totalAmount = {totalAmount}
+
+    promocode.addEventListener('change', function (e) {
+      if (e.target.value === "nodobra") {
+        alert('dziala');
+        correctpromocode = 1;
+      }
+    });
+
+    categoryselect.addEventListener('change', function (e) {
+      if (e.target.value === "1") {
+        alert('Selected');
+      }
+    });
 
     dataPromise
     .then((response) => {
@@ -33,17 +52,29 @@ const App = () => {
       setProducts(products);
     });
   }, []);
-
+  
+  
+  
+  const clearCart = () => {
+    setCart([])
+  }
+  const deleteOneItem = (product) => {
+    const tak = cart.findIndex((e) => e.id === product.id)
+    cart.splice(tak,1)
+    setCart([...cart])
+  }
   return (
     <div className="App">
       <div class="header">
       <img class="alledrogo" src={companyLogo}/>
         <div class="category">
-          <select>
-            <option value="0">Wybierz kategorie produktu:</option>
+        <input type="text" class="promocode" id="order-select" name="promocode" placeholder="Wpisz swój kod promocyjny"></input>
+          <select id="category">
+            <option value="0">Wybierz kategorie</option>
             <option value="1">Telefony</option>
             <option value="2">Laptopy</option>
           </select>
+          <input type="submit" class="submit" value="Zatwierdź"></input>
         </div>
         <div class="koszyk">
         <div class="cartbutton">
@@ -54,10 +85,10 @@ const App = () => {
           {/* <Modal.Title>Koszyk</Modal.Title> */}
         </Modal.Header>
         <Modal.Body>
-      {!!cart.length && <Cart cart={cart} />}
+      {!!cart.length && <Cart deleteOneItem={deleteOneItem} clearCart={clearCart} cart={cart} />}
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={handleClose}>
+          <Button class="closemodal" onClick={handleClose}>
             Zamknij
           </Button>
         </Modal.Footer>
--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,13 +1,17 @@
 import './Cart.css';
+import deleteicon from './delete.png';
 
 const Cart = (props) => {
     const items = props.cart;
     const itemsToDisplay = [];
     const itemsCount = {};
     let totalAmount = 0;
+    let safemoney = 0;
+    let correctpromocode = 0;
 
     items.forEach(item => {
         totalAmount += item.promoPrice ? item.promoPrice : item.price;
+        safemoney += item.price - item.promoPrice;
 
         if (!itemsCount[item.id]) {
             itemsCount[item.id] = 1;
@@ -17,15 +21,22 @@ const Cart = (props) => {
         }
     });
 
+    if (correctpromocode === 1) {
+        alert('dziala2');
+        totalAmount = totalAmount * 0.70
+    }
+
     return (
         <div className="cart">
             <h2>Koszyk</h2>
 
             {itemsToDisplay.map((item, index) => {
-                return <p key={`${item.id}-${index}`}>{item.name} {itemsCount[item.id] ? `x${itemsCount[item.id]}` : null}</p>
+                return <p key={`${item.id}-${index}`}>{item.name} {itemsCount[item.id] ? `x${itemsCount[item.id]}` : null} <img class="deleteicon" src={deleteicon} onClick={() => props.deleteOneItem(item)}></img></p>
             })}
-
             <p><b>SUMA:</b> {totalAmount}zł</p>
+            <p><b>ZAOSZCZĘDZONE PIENIĄDZE:</b> {safemoney}zł</p>
+            <button className="clearcart"
+                            onClick={() => props.clearCart()}>Wyczyść koszyk</button>
         </div>
     )
 };
